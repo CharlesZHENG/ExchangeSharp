2ce1a7d1e237bfe2ac79c6481ad8de156cf77490
chengliang_zheng <chengliang_zheng@foxmail.com>
2018-06-11 23:47:34 +0800

    代码调整使其可运行

--- BitKiwi/Program.cs
+++ BitKiwi/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using ExchangeSharp;
+using System.Threading;
 
 namespace BitKiwi
 {
@@ -22,30 +16,30 @@ namespace BitKiwi
         static void Main(string[] args)
         {
             exchange.LoadAPIKeysUnsecure("", "");
-            
-            floatamountbuy = 0m;
-            floatamountsell = 0m;
-            diffprice = 0m;
-            sleeptime = 0;
+
+            floatamountbuy = 10m;
+            floatamountsell = 10m;
+            diffprice = 0m;//
+            sleeptime = 3500;
             baseCurrency = "ht";
 
             while (true)
             {
-                OnTick("btc");
+                OnTick("xrp");
             }
         }
-        static void CancelPendingOrders(string currency)
+        static void CancelPendingOrders(string marketId)
         {
-            var orders = exchange.GetOpenOrderDetails(currency);
+            var orders = exchange.GetOpenOrderDetails(marketId);
             foreach (var orderResult in orders)
             {
                 exchange.CancelOrder(orderResult.OrderId, orderResult.Symbol);
             }
         }
         //计算将要下单的价格
-        public static decimal GetPrice(ExType type, string currency, int count = 20)//20
+        public static decimal GetPrice(ExType type, string marketid, int count = 20)//20
         {
-            var depth = exchange.GetOrderBook(currency, count);
+            var depth = exchange.GetOrderBook(marketid, count);
             decimal amountBids = 0;
             decimal amountAsks = 0;
             //计算买价，获取累计深度达到预设的价格
@@ -53,12 +47,12 @@ namespace BitKiwi
             {
                 for (var i = 0; i < 20; i++)
                 {
-                    amountBids += depth.Bids[i].Amount;
+                    amountBids += depth.Bids.ElementAt(i).Value.Amount;
                     //floatamountbuy就是预设的累计买单深度
                     if (amountBids > floatamountbuy)
                     {
                         //稍微加0.01，使得订单排在前面
-                        return depth.Bids[i].Price + 0.01m;
+                        return depth.Bids.ElementAt(i).Value.Price + 0.01m;
                     }
                 }
             }
@@ -67,21 +61,21 @@ namespace BitKiwi
             {
                 for (var j = 0; j < 20; j++)
                 {
-                    amountAsks += depth.Asks[j].Amount;
+                    amountAsks += depth.Asks.ElementAt(j).Value.Amount;
                     if (amountAsks > floatamountsell)
                     {
-                        return depth.Asks[j].Price - 0.01m;
+                        return depth.Asks.ElementAt(j).Value.Price - 0.01m;
                     }
                 }
             }
             //遍历了全部深度仍未满足需求，就返回一个价格，以免出现bug
-            return depth.Asks[0].Price;
+            return depth.Asks.ElementAt(0).Value.Price;
         }
 
         public static void OnTick(string currency)
         {
-            var buyPrice = GetPrice(ExType.Buy, currency);
-            var sellPrice = GetPrice(ExType.Sell, currency);
+            var buyPrice = GetPrice(ExType.Buy, currency + baseCurrency);
+            var sellPrice = GetPrice(ExType.Sell, currency + baseCurrency);
             //买卖价差如果小于预设值diffprice，就会挂一个相对更深的价格
             if ((sellPrice - buyPrice) <= diffprice)
             {
@@ -89,13 +83,13 @@ namespace BitKiwi
                 sellPrice += 10;
             }
             //把原有的单子全部撤销，实际上经常出现新的价格和已挂单价格相同的情况，此时不需要撤销
-            CancelPendingOrders(currency);
+            CancelPendingOrders(currency + baseCurrency);
             //获取账户信息，确定目前账户存在多少钱和多少币
             var account = exchange.GetAmountsAvailableToTrade();
-            //可买的比特币量
-            var amountBuy = SelfMath.ToFixed((account[baseCurrency] / buyPrice - 0.1m), 2);
+            //可买的比特币量            
+            var amountBuy = SelfMath.ToFixed((account.TryGetValueOrDefault(baseCurrency, 0) / buyPrice - 0.1m), 2);
             //可卖的比特币量，注意到没有仓位的限制，有多少就买卖多少，因为我当时的钱很少
-            var amountSell = SelfMath.ToFixed(account[currency], 2);
+            var amountSell = SelfMath.ToFixed(account.TryGetValueOrDefault(currency, 0), 2);
             if (amountSell > 0.02m)
             {
                 exchange.PlaceOrder(new ExchangeOrderRequest() { Amount = amountSell, IsBuy = false, Price = sellPrice });
